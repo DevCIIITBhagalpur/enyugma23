d8290c23e4361d68147cab9e775af3333104c4dc
USERSATOSHI <user.satoshi69@gmail.com>
2023-07-09 00:21:59 +0530

    base updates

--- enyugma-frontend/src/App.jsx
+++ enyugma-frontend/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home/index.jsx';
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={
+                  <Home />
+                } />
+                <Route path="/events" element={<h1>About</h1>} />
+                <Route path="/" element={<h1>Contact</h1>} />
+            </Routes>
+        </div>
+    )
 }
 
 export default App
---
+++ enyugma-frontend/src/Pages/Home/index.jsx
@@ -0,0 +1,6 @@
+import "./index.scss";
+export default function Home() {
+    return (
+        <div className="home"></div>
+    )
+}
