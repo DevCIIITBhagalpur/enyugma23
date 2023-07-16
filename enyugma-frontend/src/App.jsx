ed0c28167a458d554b7dfe0cd6a01560ce7e4696
gangwatsomya <gangwarsomya16@gmail.com>
2023-07-16 23:14:06 +0530

    added footer section

--- enyugma-frontend/src/App.jsx
+++ enyugma-frontend/src/App.jsx
@@ -2,9 +2,11 @@
 import './App.scss'
 import { Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home/index.jsx';
+import Footer from './Components/Footer/Footer';
 function App() {
     return (
         <div className="App">
+            <Footer/>
             <Routes>
                 <Route path="/" element={
                   <Home />
@@ -12,6 +14,7 @@ function App() {
                 <Route path="/events" element={<h1>About</h1>} />
                 <Route path="/" element={<h1>Contact</h1>} />
             </Routes>
+            
         </div>
     )
 }
