a8fd5ecaad23fc046acc128c8f0e24b9c6f9c4cf
USERSATOSHI <user.satoshi69@gmail.com>
2023-07-19 17:53:53 +0530

    changes

--- enyugma-frontend/src/App.jsx
+++ enyugma-frontend/src/App.jsx
@@ -1,28 +1,10 @@
-<<<<<<< HEAD
 
-import './App.scss'
-import { Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home/index.jsx';
-import Footer from './Components/Footer/Footer';
-function App() {
-    return (
-        <div className="App">
-            <Footer/>
-            <Routes>
-                <Route path="/" element={
-                  <Home />
-                } />
-                <Route path="/events" element={<h1>About</h1>} />
-                <Route path="/" element={<h1>Contact</h1>} />
-            </Routes>
-            
-        </div>
-    )
-=======
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/index.jsx";
+import GlowBall from "./Components/cursor/index.jsx";
 function App() {
+
   return (
     <div className="App">
       <Routes>
@@ -32,7 +14,6 @@ function App() {
       </Routes>
     </div>
   );
->>>>>>> bed4e68c55a113522cfcb663a5f594510f4b7565
 }
 
 export default App;
--- enyugma-frontend/src/Pages/Home/index.jsx
+++ enyugma-frontend/src/Pages/Home/index.jsx
@@ -3,48 +3,56 @@ import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
+import { Link } from "react-router-dom";
 import CountdownTimer from "../../Components/CountdownTimer";
+import GlowBall from "../../Components/cursor/index.jsx";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: "transparent",
-  padding: theme.spacing(4),
-  textAlign: "center",
+    backgroundColor: "transparent",
+    padding: theme.spacing(4),
+    textAlign: "center",
 }));
 
 export default function Home() {
-  return (
-    <div className="home">
-      <Box sx={{ width: "100%" }}>
-        <Grid
-          container
-          rowSpacing={1}
-          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-          columns={{ xs: 6, md: 12 }}
-        >
-          <Grid item xs={6}>
-            <Item>
-              <img className="banner" src="./src/assets/banner.webp" />
-            </Item>
-          </Grid>
-          <Grid item xs={6}>
-            <Item>
-              <div className="model">3D model</div>
-            </Item>
-          </Grid>
-          <Grid item xs={6}>
-            <Item className="buttons">
-              <button className="btn">Explore</button>
-              <button className="btn">Events</button>
-            </Item>
-          </Grid>
-          <Grid item xs={6}>
-            <Item className="countDownSection">
-              <p className="countDownText">The countdown has begun !</p>
-              <CountdownTimer />
-            </Item>
-          </Grid>
-        </Grid>
-      </Box>
-    </div>
-  );
+    return (
+        <div className="home">
+            <GlowBall />
+            <Box sx={{ width: "100%" }}>
+                <Grid
+                    container
+                    rowSpacing={1}
+                    columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+                    columns={{ xs: 6, md: 12 }}
+                >
+                    <Grid item xs={6}>
+                        <Item className="bannerItem">
+                        </Item>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Item>
+                            <div className="model">3D model</div>
+                        </Item>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Item className="buttons">
+                            <Link className="btn" to="/explore">
+                                Explore
+                            </Link>
+                            <Link className="btn" to="/events">
+                                Events
+                            </Link>
+                        </Item>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Item className="countDownSection">
+                            <p className="countDownText">
+                                The countdown has begun !
+                            </p>
+                            <CountdownTimer />
+                        </Item>
+                    </Grid>
+                </Grid>
+            </Box>
+        </div>
+    );
 }
