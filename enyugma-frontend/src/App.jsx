3a5bb5717b645ceb2ef67ebdddbed4323f844384
USERSATOSHI <user.satoshi69@gmail.com>
2023-07-19 17:56:23 +0530

    update

--- enyugma-frontend/src/App.jsx
+++ enyugma-frontend/src/App.jsx
@@ -3,10 +3,12 @@ import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/index.jsx";
 import GlowBall from "./Components/cursor/index.jsx";
+
 function App() {
 
   return (
     <div className="App">
+      <GlowBall />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/events" element={<h1>About</h1>} />
