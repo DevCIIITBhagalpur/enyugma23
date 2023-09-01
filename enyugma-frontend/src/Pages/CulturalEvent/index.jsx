c7a7a94956ed2745e43a8df59b3ef8a1b2c2211d
Shaikh_Abdullah <abdullah13112004@gmail.com>
2023-09-01 20:01:51 +0530

    Added file structure for event pages

---
+++ enyugma-frontend/src/Pages/CulturalEvent/index.jsx
@@ -0,0 +1,6 @@
+import "./index.scss";
+import { useState } from "react";
+
+export default function CulturalEvent() {
+  return <div className="culturalEvent"></div>;
+}
---
+++ enyugma-frontend/src/Pages/TechnicalEvent/index.jsx
@@ -0,0 +1,6 @@
+import "./index.scss";
+import { useState } from "react";
+
+export default function TechnicalEvent() {
+  return <div className="technicalEvent"></div>;
+}
