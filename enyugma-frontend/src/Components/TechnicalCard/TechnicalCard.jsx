0adc80b086c73d00d199c4a210fa982aca6aad45
Shaikh_Abdullah <abdullah13112004@gmail.com>
2023-09-03 12:28:51 +0530

    Creat Object for technical Event. Map the object

--- enyugma-frontend/src/Components/TechnicalCard/TechnicalCard.jsx
+++ enyugma-frontend/src/Components/TechnicalCard/TechnicalCard.jsx
@@ -1,23 +1,24 @@
-const TechnicalCard = ({eventName, eventDesc, imageSource}) => {
-        return  (
-            <div class="card" elevation={8}>
-            <img
-                class="avatar"
-                width="300"
-                height="480"
-                src={imageSource}
-                alt="avatar"/>
-            <div class="creditoverlay"></div>
-            <div class="info">
-                <h1>{eventName}</h1>
-                <p>{eventDesc}</p>
-            </div>
-            </div> 
-    )
-} 
+/* eslint-disable react/prop-types */
+import "./TechnicalCard.scss";
+import { React, useEffect } from "react";
+
+const TechnicalCard = (props) => {
+  return (
+    <div className="card" elevation={8}>
+      <img
+        className="avatar"
+        width="300"
+        height="480"
+        src={props.imgUrl}
+        alt="avatar"
+      />
+      <div className="creditoverlay"></div>
+      <div className="info">
+        <h1>{props.eventName}</h1>
+        <p>{props.eventDesc}</p>
+      </div>
+    </div>
+  );
+};
 
 export default TechnicalCard;
-
-
-
-
--- enyugma-frontend/src/Pages/TechnicalEvent/index.jsx
+++ enyugma-frontend/src/Pages/TechnicalEvent/index.jsx
@@ -1,6 +1,33 @@
 import "./index.scss";
 import { useState } from "react";
+import technicalEvent from "./technicalEvent";
+import TechnicalCard from "../../Components/TechnicalCard/TechnicalCard";
+import Navbar from "../../Components/Navbar";
+
+function createTechnicalCard(technicalEvent) {
+  return (
+    <TechnicalCard
+      id={technicalEvent.id}
+      key={technicalEvent.id}
+      eventName={technicalEvent.eventName}
+      eventDesc={technicalEvent.eventDesc}
+      imgUrl={technicalEvent.imgUrl}
+    />
+  );
+}
 
 export default function TechnicalEvent() {
-  return <div className="technicalEvent"></div>;
+  return (
+    <>
+      <Navbar />
+      <div className="technicalEvent">
+        {technicalEvent.map(createTechnicalCard)}
+        <TechnicalCard
+          eventName="Codeloop"
+          eventDesc="lorem ipsum 50 jassdiajlskmx sdfncasjkbcasa kansjknx"
+          imgUrl="https://d8it4huxumps7.cloudfront.net/uploads/images/opportunity/banner/638b5a18b4907_codeloop.png?d=1920x1920"
+        />
+      </div>
+    </>
+  );
 }
