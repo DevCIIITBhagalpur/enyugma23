50d7f88c7ed69a851d01d4014ffa6e6d363b9bef
Shaikh_Abdullah <abdullah13112004@gmail.com>
2023-09-11 09:26:29 +0530

    update cultural card

--- enyugma-frontend/src/Components/CulturalCard/CulturalCard.jsx
+++ enyugma-frontend/src/Components/CulturalCard/CulturalCard.jsx
@@ -1,32 +1,30 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import border from "../../assets/border.png";
 import "./CulturalCard.scss";
-const CulturalCard = ({ imgUrl, eventName, eventDesc, ...others }) => {
-    return (
-        <div
-            className="card"
-            {...others}
-            style={{
-                backgroundImage: `url(${imgUrl})`,
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                borderRadius: "8px",
-            }}
-        >
-            <img
-                src={border}
-                alt="bg-removebg-preview"
-                border="0"
-                className="bg"
-            />
-            <div className="overlay">
-                <h1 className="title">{eventName}</h1>
-                <p>{eventDesc}</p>
-                <button>Register</button>
-            </div>
-        </div>
-    );
+
+//{ imgUrl, eventName, eventDesc, ...others }
+const CulturalCard = (props) => {
+  return (
+    <div
+      className="card"
+      {...props.others}
+      style={{
+        backgroundImage: `url(${props.imgUrl})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        borderRadius: "8px",
+      }}
+    >
+      <img src={border} alt="bg-removebg-preview" border="0" className="bg" />
+      <div className="overlay">
+        <h1 className="title">{props.eventName}</h1>
+        <p>{props.eventDesc}</p>
+        <button>Register</button>
+      </div>
+    </div>
+  );
 };
 
 export default CulturalCard;
